8473f66f25b514e68b968d50bf13c66651017440
Karina Barinova <kbarinova11@gmail.com>
2023-03-10 17:17:32 +0000

    Maze solve algorithm

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,59 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1]
+]
 
+function walkMaze(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][], path: Point[]): boolean {
+    //1. Base Case
+    //off the map
+    if (curr.x < 0 || curr.x >= maze[0].length ||
+        curr.y < 0 || curr.y >= maze.length) {
+        return false;
+    }
+
+    //on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    //at the end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(end)
+        return true
+    }
+
+    //we have seen this
+    if (seen[curr.y][curr.x]) {
+        return false
+    }
+
+    seen[curr.y][curr.x] = true
+    path.push(curr)
+
+    for (let i = 0; i < dir.length; ++i) {
+        const [x, y] = dir[i]
+        if (walkMaze(maze, wall, {
+            x: curr.x + x,
+            y: curr.y + y
+        }, end, seen, path)) {
+            return true;
+        }
+    }
+
+    path.pop();
+
+    return false
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+    const seen: boolean[][] = []
+    const path: Point[] = []
+    for (let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[0].length).fill(false))
+    }
+
+    walkMaze(maze, wall, start, end, seen, path)
+    return path
 }
